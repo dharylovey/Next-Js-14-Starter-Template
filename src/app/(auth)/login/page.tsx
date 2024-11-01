cd7694f75aee42c46b965caa752cdf948bda82b6
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-11-01 19:50:12 +0800

    ✅Install Shadcn Card and Form

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center text-3xl">
+      LoginPage
+    </div>
+  );
+}
